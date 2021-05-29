dfc2688eb5d20b2235dd9ae7927d2b4e699f3837
JanBaran1 <jan.baran@pl.abb.com>
2021-05-29 19:36:11 +0200

    Add project files.

---
+++ GUI.cpp
@@ -0,0 +1,179 @@
+﻿///////////////////////////////////////////////////////////////////////////
+// C++ code generated with wxFormBuilder (version Jun 17 2015)
+// http://www.wxformbuilder.org/
+//
+// PLEASE DO "NOT" EDIT THIS FILE!
+///////////////////////////////////////////////////////////////////////////
+
+#include "GUI.h"
+
+///////////////////////////////////////////////////////////////////////////
+
+MyFrame1::MyFrame1( wxWindow* parent, wxWindowID id, const wxString& title, const wxPoint& pos, const wxSize& size, long style ) : wxFrame( parent, id, title, pos, size, style )
+{
+	this->SetSizeHints( wxDefaultSize, wxDefaultSize );
+	
+	wxBoxSizer* bSizer1;
+	bSizer1 = new wxBoxSizer( wxHORIZONTAL );
+	
+	wxBoxSizer* bSizer2;
+	bSizer2 = new wxBoxSizer( wxVERTICAL );
+	
+	m_panel3 = new wxPanel( this, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxTAB_TRAVERSAL );
+	m_panel3->SetBackgroundColour( wxSystemSettings::GetColour( wxSYS_COLOUR_HIGHLIGHTTEXT ) );
+	
+	bSizer2->Add( m_panel3, 1, wxEXPAND | wxALL, 5 );
+	
+	
+	bSizer1->Add( bSizer2, 1, wxEXPAND, 5 );
+	
+	wxBoxSizer* bSizer4;
+	bSizer4 = new wxBoxSizer( wxVERTICAL );
+	
+	m_panel4 = new wxPanel( this, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxTAB_TRAVERSAL );
+	m_panel4->SetBackgroundColour( wxSystemSettings::GetColour( wxSYS_COLOUR_HIGHLIGHTTEXT ) );
+	m_panel4->SetMinSize( wxSize( 200,-1 ) );
+	m_panel4->SetMaxSize( wxSize( -1,200 ) );
+	
+	bSizer4->Add( m_panel4, 1, wxEXPAND | wxALL, 5 );
+	
+	m_panel5 = new wxPanel( this, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxTAB_TRAVERSAL );
+	m_panel5->SetBackgroundColour( wxSystemSettings::GetColour( wxSYS_COLOUR_HIGHLIGHTTEXT ) );
+	m_panel5->SetMaxSize( wxSize( -1,200 ) );
+	
+	bSizer4->Add( m_panel5, 1, wxEXPAND | wxALL, 5 );
+	
+	
+	bSizer1->Add( bSizer4, 0, wxEXPAND, 5 );
+	
+	wxBoxSizer* bSizer3;
+	bSizer3 = new wxBoxSizer( wxVERTICAL );
+	
+	m_button1 = new wxButton( this, wxID_ANY, wxT("Wczytaj obrazek"), wxDefaultPosition, wxDefaultSize, 0 );
+	bSizer3->Add( m_button1, 0, wxALIGN_CENTER|wxALL, 5 );
+	
+	m_staticText2 = new wxStaticText( this, wxID_ANY, wxT("Proporcjonalność"), wxDefaultPosition, wxDefaultSize, 0 );
+	m_staticText2->Wrap( -1 );
+	bSizer3->Add( m_staticText2, 0, wxALIGN_CENTER|wxALL, 5 );
+	
+	m_slider1 = new wxSlider( this, wxID_ANY, 50, 0, 100, wxDefaultPosition, wxDefaultSize, wxSL_HORIZONTAL );
+	bSizer3->Add( m_slider1, 0, wxALIGN_CENTER|wxALL, 5 );
+	
+	m_staticText3 = new wxStaticText( this, wxID_ANY, wxT("Siła zmian"), wxDefaultPosition, wxDefaultSize, 0 );
+	m_staticText3->Wrap( -1 );
+	bSizer3->Add( m_staticText3, 0, wxALIGN_CENTER|wxALL, 5 );
+	
+	m_slider2 = new wxSlider( this, wxID_ANY, 50, 0, 100, wxDefaultPosition, wxDefaultSize, wxSL_HORIZONTAL );
+	bSizer3->Add( m_slider2, 0, wxALIGN_CENTER|wxALL, 5 );
+	
+	m_staticText4 = new wxStaticText( this, wxID_ANY, wxT("Jasność"), wxDefaultPosition, wxDefaultSize, 0 );
+	m_staticText4->Wrap( -1 );
+	bSizer3->Add( m_staticText4, 0, wxALIGN_CENTER|wxALL, 5 );
+	
+	m_slider3 = new wxSlider( this, wxID_ANY, 50, 0, 100, wxDefaultPosition, wxDefaultSize, wxSL_HORIZONTAL );
+	bSizer3->Add( m_slider3, 0, wxALIGN_CENTER|wxALL, 5 );
+	
+	m_staticText5 = new wxStaticText( this, wxID_ANY, wxT("Nasycenie"), wxDefaultPosition, wxDefaultSize, 0 );
+	m_staticText5->Wrap( -1 );
+	bSizer3->Add( m_staticText5, 0, wxALIGN_CENTER|wxALL, 5 );
+	
+	m_slider4 = new wxSlider( this, wxID_ANY, 50, 0, 100, wxDefaultPosition, wxDefaultSize, wxSL_HORIZONTAL );
+	bSizer3->Add( m_slider4, 0, wxALIGN_CENTER|wxALL, 5 );
+	
+	
+	bSizer1->Add( bSizer3, 0, wxEXPAND, 5 );
+	
+	
+	this->SetSizer( bSizer1 );
+	this->Layout();
+	
+	this->Centre( wxBOTH );
+	
+	// Connect Events
+	m_panel3->Connect( wxEVT_UPDATE_UI, wxUpdateUIEventHandler( MyFrame1::m_panel3OnUpdateUI ), NULL, this );
+	m_panel4->Connect( wxEVT_UPDATE_UI, wxUpdateUIEventHandler( MyFrame1::m_panel4OnUpdateUI ), NULL, this );
+	m_panel5->Connect( wxEVT_UPDATE_UI, wxUpdateUIEventHandler( MyFrame1::m_panel5OnUpdateUI ), NULL, this );
+	m_button1->Connect( wxEVT_COMMAND_BUTTON_CLICKED, wxCommandEventHandler( MyFrame1::m_button1OnButtonClick ), NULL, this );
+	m_slider1->Connect( wxEVT_SCROLL_TOP, wxScrollEventHandler( MyFrame1::m_slider1OnScroll ), NULL, this );
+	m_slider1->Connect( wxEVT_SCROLL_BOTTOM, wxScrollEventHandler( MyFrame1::m_slider1OnScroll ), NULL, this );
+	m_slider1->Connect( wxEVT_SCROLL_LINEUP, wxScrollEventHandler( MyFrame1::m_slider1OnScroll ), NULL, this );
+	m_slider1->Connect( wxEVT_SCROLL_LINEDOWN, wxScrollEventHandler( MyFrame1::m_slider1OnScroll ), NULL, this );
+	m_slider1->Connect( wxEVT_SCROLL_PAGEUP, wxScrollEventHandler( MyFrame1::m_slider1OnScroll ), NULL, this );
+	m_slider1->Connect( wxEVT_SCROLL_PAGEDOWN, wxScrollEventHandler( MyFrame1::m_slider1OnScroll ), NULL, this );
+	m_slider1->Connect( wxEVT_SCROLL_THUMBTRACK, wxScrollEventHandler( MyFrame1::m_slider1OnScroll ), NULL, this );
+	m_slider1->Connect( wxEVT_SCROLL_THUMBRELEASE, wxScrollEventHandler( MyFrame1::m_slider1OnScroll ), NULL, this );
+	m_slider1->Connect( wxEVT_SCROLL_CHANGED, wxScrollEventHandler( MyFrame1::m_slider1OnScroll ), NULL, this );
+	m_slider2->Connect( wxEVT_SCROLL_TOP, wxScrollEventHandler( MyFrame1::m_slider2OnScroll ), NULL, this );
+	m_slider2->Connect( wxEVT_SCROLL_BOTTOM, wxScrollEventHandler( MyFrame1::m_slider2OnScroll ), NULL, this );
+	m_slider2->Connect( wxEVT_SCROLL_LINEUP, wxScrollEventHandler( MyFrame1::m_slider2OnScroll ), NULL, this );
+	m_slider2->Connect( wxEVT_SCROLL_LINEDOWN, wxScrollEventHandler( MyFrame1::m_slider2OnScroll ), NULL, this );
+	m_slider2->Connect( wxEVT_SCROLL_PAGEUP, wxScrollEventHandler( MyFrame1::m_slider2OnScroll ), NULL, this );
+	m_slider2->Connect( wxEVT_SCROLL_PAGEDOWN, wxScrollEventHandler( MyFrame1::m_slider2OnScroll ), NULL, this );
+	m_slider2->Connect( wxEVT_SCROLL_THUMBTRACK, wxScrollEventHandler( MyFrame1::m_slider2OnScroll ), NULL, this );
+	m_slider2->Connect( wxEVT_SCROLL_THUMBRELEASE, wxScrollEventHandler( MyFrame1::m_slider2OnScroll ), NULL, this );
+	m_slider2->Connect( wxEVT_SCROLL_CHANGED, wxScrollEventHandler( MyFrame1::m_slider2OnScroll ), NULL, this );
+	m_slider3->Connect( wxEVT_SCROLL_TOP, wxScrollEventHandler( MyFrame1::m_slider3OnScroll ), NULL, this );
+	m_slider3->Connect( wxEVT_SCROLL_BOTTOM, wxScrollEventHandler( MyFrame1::m_slider3OnScroll ), NULL, this );
+	m_slider3->Connect( wxEVT_SCROLL_LINEUP, wxScrollEventHandler( MyFrame1::m_slider3OnScroll ), NULL, this );
+	m_slider3->Connect( wxEVT_SCROLL_LINEDOWN, wxScrollEventHandler( MyFrame1::m_slider3OnScroll ), NULL, this );
+	m_slider3->Connect( wxEVT_SCROLL_PAGEUP, wxScrollEventHandler( MyFrame1::m_slider3OnScroll ), NULL, this );
+	m_slider3->Connect( wxEVT_SCROLL_PAGEDOWN, wxScrollEventHandler( MyFrame1::m_slider3OnScroll ), NULL, this );
+	m_slider3->Connect( wxEVT_SCROLL_THUMBTRACK, wxScrollEventHandler( MyFrame1::m_slider3OnScroll ), NULL, this );
+	m_slider3->Connect( wxEVT_SCROLL_THUMBRELEASE, wxScrollEventHandler( MyFrame1::m_slider3OnScroll ), NULL, this );
+	m_slider3->Connect( wxEVT_SCROLL_CHANGED, wxScrollEventHandler( MyFrame1::m_slider3OnScroll ), NULL, this );
+	m_slider4->Connect( wxEVT_SCROLL_TOP, wxScrollEventHandler( MyFrame1::m_slider4OnScroll ), NULL, this );
+	m_slider4->Connect( wxEVT_SCROLL_BOTTOM, wxScrollEventHandler( MyFrame1::m_slider4OnScroll ), NULL, this );
+	m_slider4->Connect( wxEVT_SCROLL_LINEUP, wxScrollEventHandler( MyFrame1::m_slider4OnScroll ), NULL, this );
+	m_slider4->Connect( wxEVT_SCROLL_LINEDOWN, wxScrollEventHandler( MyFrame1::m_slider4OnScroll ), NULL, this );
+	m_slider4->Connect( wxEVT_SCROLL_PAGEUP, wxScrollEventHandler( MyFrame1::m_slider4OnScroll ), NULL, this );
+	m_slider4->Connect( wxEVT_SCROLL_PAGEDOWN, wxScrollEventHandler( MyFrame1::m_slider4OnScroll ), NULL, this );
+	m_slider4->Connect( wxEVT_SCROLL_THUMBTRACK, wxScrollEventHandler( MyFrame1::m_slider4OnScroll ), NULL, this );
+	m_slider4->Connect( wxEVT_SCROLL_THUMBRELEASE, wxScrollEventHandler( MyFrame1::m_slider4OnScroll ), NULL, this );
+	m_slider4->Connect( wxEVT_SCROLL_CHANGED, wxScrollEventHandler( MyFrame1::m_slider4OnScroll ), NULL, this );
+}
+
+MyFrame1::~MyFrame1()
+{
+	// Disconnect Events
+	m_panel3->Disconnect( wxEVT_UPDATE_UI, wxUpdateUIEventHandler( MyFrame1::m_panel3OnUpdateUI ), NULL, this );
+	m_panel4->Disconnect( wxEVT_UPDATE_UI, wxUpdateUIEventHandler( MyFrame1::m_panel4OnUpdateUI ), NULL, this );
+	m_panel5->Disconnect( wxEVT_UPDATE_UI, wxUpdateUIEventHandler( MyFrame1::m_panel5OnUpdateUI ), NULL, this );
+	m_button1->Disconnect( wxEVT_COMMAND_BUTTON_CLICKED, wxCommandEventHandler( MyFrame1::m_button1OnButtonClick ), NULL, this );
+	m_slider1->Disconnect( wxEVT_SCROLL_TOP, wxScrollEventHandler( MyFrame1::m_slider1OnScroll ), NULL, this );
+	m_slider1->Disconnect( wxEVT_SCROLL_BOTTOM, wxScrollEventHandler( MyFrame1::m_slider1OnScroll ), NULL, this );
+	m_slider1->Disconnect( wxEVT_SCROLL_LINEUP, wxScrollEventHandler( MyFrame1::m_slider1OnScroll ), NULL, this );
+	m_slider1->Disconnect( wxEVT_SCROLL_LINEDOWN, wxScrollEventHandler( MyFrame1::m_slider1OnScroll ), NULL, this );
+	m_slider1->Disconnect( wxEVT_SCROLL_PAGEUP, wxScrollEventHandler( MyFrame1::m_slider1OnScroll ), NULL, this );
+	m_slider1->Disconnect( wxEVT_SCROLL_PAGEDOWN, wxScrollEventHandler( MyFrame1::m_slider1OnScroll ), NULL, this );
+	m_slider1->Disconnect( wxEVT_SCROLL_THUMBTRACK, wxScrollEventHandler( MyFrame1::m_slider1OnScroll ), NULL, this );
+	m_slider1->Disconnect( wxEVT_SCROLL_THUMBRELEASE, wxScrollEventHandler( MyFrame1::m_slider1OnScroll ), NULL, this );
+	m_slider1->Disconnect( wxEVT_SCROLL_CHANGED, wxScrollEventHandler( MyFrame1::m_slider1OnScroll ), NULL, this );
+	m_slider2->Disconnect( wxEVT_SCROLL_TOP, wxScrollEventHandler( MyFrame1::m_slider2OnScroll ), NULL, this );
+	m_slider2->Disconnect( wxEVT_SCROLL_BOTTOM, wxScrollEventHandler( MyFrame1::m_slider2OnScroll ), NULL, this );
+	m_slider2->Disconnect( wxEVT_SCROLL_LINEUP, wxScrollEventHandler( MyFrame1::m_slider2OnScroll ), NULL, this );
+	m_slider2->Disconnect( wxEVT_SCROLL_LINEDOWN, wxScrollEventHandler( MyFrame1::m_slider2OnScroll ), NULL, this );
+	m_slider2->Disconnect( wxEVT_SCROLL_PAGEUP, wxScrollEventHandler( MyFrame1::m_slider2OnScroll ), NULL, this );
+	m_slider2->Disconnect( wxEVT_SCROLL_PAGEDOWN, wxScrollEventHandler( MyFrame1::m_slider2OnScroll ), NULL, this );
+	m_slider2->Disconnect( wxEVT_SCROLL_THUMBTRACK, wxScrollEventHandler( MyFrame1::m_slider2OnScroll ), NULL, this );
+	m_slider2->Disconnect( wxEVT_SCROLL_THUMBRELEASE, wxScrollEventHandler( MyFrame1::m_slider2OnScroll ), NULL, this );
+	m_slider2->Disconnect( wxEVT_SCROLL_CHANGED, wxScrollEventHandler( MyFrame1::m_slider2OnScroll ), NULL, this );
+	m_slider3->Disconnect( wxEVT_SCROLL_TOP, wxScrollEventHandler( MyFrame1::m_slider3OnScroll ), NULL, this );
+	m_slider3->Disconnect( wxEVT_SCROLL_BOTTOM, wxScrollEventHandler( MyFrame1::m_slider3OnScroll ), NULL, this );
+	m_slider3->Disconnect( wxEVT_SCROLL_LINEUP, wxScrollEventHandler( MyFrame1::m_slider3OnScroll ), NULL, this );
+	m_slider3->Disconnect( wxEVT_SCROLL_LINEDOWN, wxScrollEventHandler( MyFrame1::m_slider3OnScroll ), NULL, this );
+	m_slider3->Disconnect( wxEVT_SCROLL_PAGEUP, wxScrollEventHandler( MyFrame1::m_slider3OnScroll ), NULL, this );
+	m_slider3->Disconnect( wxEVT_SCROLL_PAGEDOWN, wxScrollEventHandler( MyFrame1::m_slider3OnScroll ), NULL, this );
+	m_slider3->Disconnect( wxEVT_SCROLL_THUMBTRACK, wxScrollEventHandler( MyFrame1::m_slider3OnScroll ), NULL, this );
+	m_slider3->Disconnect( wxEVT_SCROLL_THUMBRELEASE, wxScrollEventHandler( MyFrame1::m_slider3OnScroll ), NULL, this );
+	m_slider3->Disconnect( wxEVT_SCROLL_CHANGED, wxScrollEventHandler( MyFrame1::m_slider3OnScroll ), NULL, this );
+	m_slider4->Disconnect( wxEVT_SCROLL_TOP, wxScrollEventHandler( MyFrame1::m_slider4OnScroll ), NULL, this );
+	m_slider4->Disconnect( wxEVT_SCROLL_BOTTOM, wxScrollEventHandler( MyFrame1::m_slider4OnScroll ), NULL, this );
+	m_slider4->Disconnect( wxEVT_SCROLL_LINEUP, wxScrollEventHandler( MyFrame1::m_slider4OnScroll ), NULL, this );
+	m_slider4->Disconnect( wxEVT_SCROLL_LINEDOWN, wxScrollEventHandler( MyFrame1::m_slider4OnScroll ), NULL, this );
+	m_slider4->Disconnect( wxEVT_SCROLL_PAGEUP, wxScrollEventHandler( MyFrame1::m_slider4OnScroll ), NULL, this );
+	m_slider4->Disconnect( wxEVT_SCROLL_PAGEDOWN, wxScrollEventHandler( MyFrame1::m_slider4OnScroll ), NULL, this );
+	m_slider4->Disconnect( wxEVT_SCROLL_THUMBTRACK, wxScrollEventHandler( MyFrame1::m_slider4OnScroll ), NULL, this );
+	m_slider4->Disconnect( wxEVT_SCROLL_THUMBRELEASE, wxScrollEventHandler( MyFrame1::m_slider4OnScroll ), NULL, this );
+	m_slider4->Disconnect( wxEVT_SCROLL_CHANGED, wxScrollEventHandler( MyFrame1::m_slider4OnScroll ), NULL, this );
+	
+}
---
+++ GUIMyFrame1.cpp
@@ -0,0 +1,71 @@
+﻿#include "GUIMyFrame1.h"
+
+GUIMyFrame1::GUIMyFrame1( wxWindow* parent )
+:
+MyFrame1( parent )
+{
+    Bind(wxEVT_PAINT, &GUIMyFrame1::DrawPicture, this);
+    SetBackgroundStyle(wxBG_STYLE_PAINT);
+    MyImage.AddHandler(new wxJPEGHandler);
+    MyImage.AddHandler(new wxPNGHandler);
+}
+
+void GUIMyFrame1::m_panel3OnUpdateUI( wxUpdateUIEvent& event )
+{
+// TODO: Implement m_panel3OnUpdateUI
+}
+
+void GUIMyFrame1::m_panel4OnUpdateUI( wxUpdateUIEvent& event )
+{
+// TODO: Implement m_panel4OnUpdateUI
+}
+
+void GUIMyFrame1::m_panel5OnUpdateUI( wxUpdateUIEvent& event )
+{
+// TODO: Implement m_panel5OnUpdateUI
+}
+
+void GUIMyFrame1::m_button1OnButtonClick( wxCommandEvent& event )
+{
+    std::shared_ptr<wxFileDialog> WxOpenFileDialog1(new wxFileDialog(this, _("Choose a file"), _(""), _(""), _("JPEG files (*.jpg)|*.jpg"), wxFD_OPEN));
+    if (WxOpenFileDialog1->ShowModal() == wxID_OK)
+    {
+        if (!MyImage.LoadFile(WxOpenFileDialog1->GetPath(), wxBITMAP_TYPE_JPEG))
+            wxLogError(_("Nie można załadować obrazka"));
+        else
+        {
+            wxImage TempImg(MyImage);
+            TempImg.Rescale(120, 80);
+            MyImage.Paste(TempImg, MyImage.GetWidth() - 120, 0);
+            MyBitmap = wxBitmap(MyImage);
+        }
+        if (MyBitmap.Ok()) this->SetTitle(WxOpenFileDialog1->GetPath());
+        Refresh();
+    }
+}
+
+void GUIMyFrame1::m_slider1OnScroll( wxScrollEvent& event )
+{
+// TODO: Implement m_slider1OnScroll
+}
+
+void GUIMyFrame1::m_slider2OnScroll( wxScrollEvent& event )
+{
+// TODO: Implement m_slider2OnScroll
+}
+
+void GUIMyFrame1::m_slider3OnScroll( wxScrollEvent& event )
+{
+// TODO: Implement m_slider3OnScroll
+}
+
+void GUIMyFrame1::m_slider4OnScroll( wxScrollEvent& event )
+{
+// TODO: Implement m_slider4OnScroll
+}
+
+void GUIMyFrame1::DrawPicture(wxPaintEvent& e)
+{
+    wxAutoBufferedPaintDC MyDC(this);
+    if (MyBitmap.Ok()) MyDC.DrawBitmap(MyBitmap, 20, 20);
+}
