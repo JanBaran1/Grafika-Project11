f8159ed13aeaaeda9d91fe2979daa70c9afafd27
JR-agh <jacek.rybak@yahoo.com>
2021-06-10 01:20:45 +0200

    sześciokąt nie znika przy przesuwaniu slidera z paletą barw

--- ColoursHexagon.cpp
+++ ColoursHexagon.cpp
@@ -98,9 +98,9 @@ void Hexagon::drawHexagon(wxPaintEvent& event) { //do poprawy, ale dopiero, gdy 
 
 	m_bitmap = wxBitmap(m_bgImage);
 	memoryDC.SelectObject(m_bitmap);
-	memoryDC.DrawBitmap(wxBitmap(redSquare), 0, 0, true);
-	memoryDC.DrawBitmap(wxBitmap(greenSquare), 49, 27, true);
+	memoryDC.DrawBitmap(wxBitmap(greenSquare), 48, 28, true);
 	memoryDC.DrawBitmap(wxBitmap(blueSquare), -49, 27, true);
+	memoryDC.DrawBitmap(wxBitmap(redSquare), 0, 0, true);
 	wxGraphicsContext* gCon = wxGraphicsContext::Create(memoryDC);
 	if (localMaxColourValue > 75) {
 		gCon->SetPen(*wxBLACK_PEN);
